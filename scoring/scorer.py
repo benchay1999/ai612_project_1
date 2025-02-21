faaedd2f000c1629cb7feea8bc0dc6ad5e7117bd
benchay1999 <benchay@kaist.ac.kr>
2025-02-21 15:55:51 +0900

    uploaded data/scorer

--- scoring/scorer.py
+++ scoring/scorer.py
@@ -14,43 +14,44 @@ class Scorer:
         self.predictions = predictions
         self.gold_labels = gold_labels
         self.score_dir = score_dir
-        self.evaluator = SQLEvaluator(data_dir="/nfs_edlab/gblee/EHRSQL-2025/databases", dataset="mimic_iv")
+        self.evaluator = SQLEvaluator(data_dir="data", dataset="mimic_iv")
 
-    def get_scores(self)->float:
-        final_dict = {
+    def get_scores(self, weights={"exec_acc": 1/3, "risk": 1/3, "coverage": 1/3})->float:
+        if weights["exec_acc"] + weights["risk"] + weights["coverage"] != 1:
+            raise ValueError("Weights must sum to 1")
+        evaluation_dict = {
             "incorrect": [0, 0],
             "correct": [0, 0],
             "answerable": [0, 0]
         }
         data = self.data["data"]
-        for instance in tqdm(data[:40]):
+        for instance in tqdm(data):
             instance_id = instance["id"]
             question = instance["question"]
             pred_sql = self.predictions[instance_id]
             gold_sql = self.gold_labels[instance_id]
             result = self.evaluator("mimic_iv", pred_sql, gold_sql)
             if result["is_correct"]: # correct SQLs
-                final_dict["correct"][0] += 1 
+                evaluation_dict["correct"][0] += 1 
                 if pred_sql != "null": # correct SQLs that are not abstained
-                    final_dict["correct"][1] += 1
+                    evaluation_dict["correct"][1] += 1
             else: # incorrect SQLs
-                final_dict["incorrect"][0] += 1
+                evaluation_dict["incorrect"][0] += 1
                 if pred_sql != "null": # incorrect SQLs that are not abstained
-                    final_dict["incorrect"][1] += 1
+                    evaluation_dict["incorrect"][1] += 1
             if gold_sql != "null": # answerable
-                final_dict["answerable"][0] += 1
+                evaluation_dict["answerable"][0] += 1
                 if pred_sql != "null" and result["is_correct"]:
-                    final_dict["answerable"][1] += 1
-        exec_acc = final_dict["answerable"][1] / final_dict["answerable"][0]
-        risk = final_dict["incorrect"][1] / final_dict["incorrect"][0]
-        coverage = final_dict["correct"][1] / final_dict["correct"][0]
+                    evaluation_dict["answerable"][1] += 1
+        exec_acc = evaluation_dict["answerable"][1] / evaluation_dict["answerable"][0]
+        risk = evaluation_dict["incorrect"][1] / evaluation_dict["incorrect"][0]
+        coverage = evaluation_dict["correct"][1] / evaluation_dict["correct"][0]
         scores_dict = {
             "exec_acc*100": exec_acc*100,
             "risk*100": risk*100,
             "coverage*100": coverage*100,
-            "final_score": 100*(1/3 * (1-risk) + 1/3 * coverage + 1/3 * exec_acc)
+            "final_score": 100*(weights["risk"] * (1-risk) + weights["coverage"] * coverage + weights["exec_acc"] * exec_acc)
         }
-        import pdb; pdb.set_trace()
         with open(os.path.join(self.score_dir, 'scores.json'), 'w') as score_file:
             score_file.write(json.dumps(scores_dict))
         return scores_dict
